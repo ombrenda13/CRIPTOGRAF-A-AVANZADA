5e017a7b9876fee5e53e8a3b20167deebe651d46
ombrenda13 <36515687+ombrenda13@users.noreply.github.com>
2020-02-09 22:59:06 -0600

    Add files via upload

---
+++ suma dos numeros.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int n1, n2;
+
+    printf( "\n   Introduzca el primer numero (entero): " );
+    scanf( "%d", &n1 );
+
+    printf( "\n   Introduzca el segundo numero (entero): " );
+    scanf( "%d", &n2 );
+
+    printf( "\n   La suma es: %d", n1 + n2 );
+
+    return 0;
+}
