29f327788de7378feeedcb1779378964885ea859
ombrenda13 <36515687+ombrenda13@users.noreply.github.com>
2020-02-09 23:54:09 -0600

    Update suma.c

--- suma.c
+++ suma.c
@@ -1,18 +1,16 @@
 #include <stdlib.h> //Standard library
-#include <stdio.h> //Standard io
-
+#include <stdio.h> //Standard input / output
 
 int main() {
-    int n=0;
-    
-    scanf("%i%*c", &n);
- //   for(int i=0;i<n;i++){
-   //   scanf("%i%*c", &a);
-     // result=result+a;
-    //}
-  //  scanf("%i%*c", &a);
-  //  scanf("%i%*c", &b);
- //   int result = a + b;
-    printf("%d", n);
+    int a = 0;
+    int b = 0;
+
+    scanf("%i%*c", &a);
+    scanf("%i%*c", &b);
+
+    int result = a + b;
+
+    printf("%i", result);
+
     return 0;
 }
