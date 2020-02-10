636da46c0af65e01e027608b7462a57d41bca742
ombrenda13 <36515687+ombrenda13@users.noreply.github.com>
2020-02-09 23:55:11 -0600

    Update suma.c

--- suma.c
+++ suma.c
@@ -4,11 +4,16 @@
 int main() {
     int a = 0;
     int b = 0;
+        int c= 0;
+
+        int d = 0;
+
 
     scanf("%i%*c", &a);
     scanf("%i%*c", &b);
-
-    int result = a + b;
+    scanf("%i%*c", &c);
+    scanf("%i%*c", &d);
+    int result = a + b+c+d;
 
     printf("%i", result);
 
