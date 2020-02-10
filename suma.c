c0f4e92b5405f956e76757171065a806a2c9e0f2
ombrenda13 <36515687+ombrenda13@users.noreply.github.com>
2020-02-09 23:59:14 -0600

    Update suma.c

--- suma.c
+++ suma.c
@@ -2,18 +2,18 @@
 #include <stdio.h> //Standard input / output
 
 int main() {
-    float a = 0;
-    float b = 0;
-      float c= 0;
+    float a = 0.0;
+    float b = 0.0;
+      float c= 0.0;
 
-        float d = 0;
+        float d = 0.0;
 
 
     scanf("%f%*c", &a);
     scanf("%f%*c", &b);
     scanf("%f%*c", &c);
     scanf("%f%*c", &d);
-    int result = a + b+c+d;
+    float result = a + b+c+d;
 
     printf("%f", result);
 
